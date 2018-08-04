846d56c1d799824a0c32cf30796078e62a54bdeb
Gabriel Ramirez <g_e_ramirez@outlook.com>
2018-08-03 21:44:07 -0500

    Go
    Go

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -16,5 +16,5 @@ func _physics_process(delta):
 	
 func _movement():
 	if Input.is_action_pressed("ui_up"):
-		move_and_collide()
+		move_and_collide(globals.UP * delta * globals.player_speed)
 	pass
