bfa748cce44f271ba8d99b1e0013988cc3362197
Gabriel Ramirez <g_e_ramirez@outlook.com>
2018-08-03 21:37:04 -0500

    Game
    Sync

---
+++ Script/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	set_physics_process(true)
+	pass
+
+
+func _physics_process(delta):
+	
+	pass
+	
+func _movement():
+	if Input.is_action_pressed("ui_up"):
+		move_and_collide()
+	pass
---
+++ Script/globals.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+const UP = Vector2(0,-1)
+const DOWN = Vector2(0,1)
+const RIGHT = Vector2(1,0)
+const LEFT = Vector2(-1,0)
+
+var player_speed = 200
