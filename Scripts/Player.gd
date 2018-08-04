7b280c76cf4c0dc51c8004767c03f91e504e06cd
Gabriel Ramirez <g_e_ramirez@outlook.com>
2018-08-03 21:45:35 -0500

    Separated Player
    Go

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,9 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
+	
 	set_physics_process(true)
 	pass
 
